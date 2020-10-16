5679f1bb661135807816bcbfea1be712aee2256d
Alejandro Mujica <alejandro.mujica@iesdoctorbalmis.com>
2020-10-16 20:13:58 +0200

    Añadir TextTrimming

--- TextoFormato/MainWindow.xaml.cs
+++ TextoFormato/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace TextoFormato
 {
@@ -25,7 +16,7 @@ namespace TextoFormato
             InitializeComponent();
         }
 
-        private void textoEntranteTextBox_TextChanged(object sender, RoutedEventArgs e)
+        private void textoEntranteTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             textoFinalTextBlock.Text = textoEntranteTextBox.Text;
         }
